9a86a0635d8a6f022c3e7b10e495c7d0237b96ec
arsts <arstsu@gmail.com>
2019-02-02 16:55:52 +0300

    added store and provider

--- src/reducers.js
+++ src/reducers.js
@@ -8,8 +8,8 @@ const initalState = {
 // Create a reducer that is a pure function.
 export const searchRobots = (state=initalState, action={}) => {
     switch(action.type){
-        case: CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchField: action.payload});
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, { searchField: action.payload});
         default:
             return state;
     }
